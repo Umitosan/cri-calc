ca4fc9d235057b76c1090013dc481fcd296ebe97
Dominic Brown <yeahdom@gmail.com>
2017-10-03 13:33:20 -0700

    add basic files and styles

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+
+// SPECS
+//
+// 31 random integers that add up to 37806, within a range from 79 to 1011
+//
+// 10 random integers that add up to 8919, within a range from 421 to 1183
+//
+
+
+
+
+////
+////
+////
+$(document).ready(function(){
+
+});
