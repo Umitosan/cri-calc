b757090c9433a9eef8e5ee4e22b027b6d5e3d17d
Dominic Brown <yeahdom@gmail.com>
2017-10-08 19:46:39 -0700

    add custom input fields and button, basic validation with err msgs

--- scripts/script.js
+++ scripts/script.js
@@ -14,13 +14,23 @@ function getSum(myArray) {
   return sum;
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
+
+  function clearOut() {
+    $("#seed-list").empty();
+    $("#sum").text("0");
+  }
+
+  function clearIn() {
+    $("#lower-bounds").val(0);
+    $("#upper-bounds").val(0);
+    $("#quantity-returned").val(0);
+  }
 
   // 31 random integers that add up to 37806, within a range from 79 to 1437
   $("#gen-btn-1").click(function() {
-    // clear old output first
-    $("#seed-list").empty();
-    $("#sum").text("0");
+    clearOut();
+
     var rndArr = [];
     var finalArr = [];
     var masterSum = 37806;
@@ -73,20 +83,19 @@ $(document).ready(function(){
 
   // 10 random integers that add up to 8919, within a range from 421 to 1183
   $("#gen-btn-2").click(function() {
-    // clear old output first
-    $("#seed-list").empty();
-    $("#sum").text("0");
-   var rndArr = [];
-   var finalArr = [];
-   var masterSum = 8919;
-   var lowBound = 421;
-   var highBound = 1183;
-   var numbersNeeded = 10;
-   var diff1 = 0;
-   var diff2 = 0;
-   var lastnum = 0;
-   var distNum = 0;
-   var distRemain = 0;
+    clearOut();
+
+    var rndArr = [];
+    var finalArr = [];
+    var masterSum = 8919;
+    var lowBound = 421;
+    var highBound = 1183;
+    var numbersNeeded = 10;
+    var diff1 = 0;
+    var diff2 = 0;
+    var lastnum = 0;
+    var distNum = 0;
+    var distRemain = 0;
 
     // populate start array values
     var tmpsum = 0;
@@ -124,6 +133,72 @@ $(document).ready(function(){
     });
     $("li:odd").css( "background-color", "lightgrey" );
     $("#sum").text(getSum(finalArr));
+  });
+
+  // Custom user input
+  $("#gen-btn-3").click(function() {
+    clearOut();
+    var userLower = parseInt($("#lower-bounds").val());
+    var userUpper = parseInt($("#upper-bounds").val());
+    var userSum = parseInt($("#desired-sum").val());
+    var userQuantity = parseInt($("#quantity-returned").val());
+    console.log("typeof userLower = ", typeof userLower);
+    console.log("");
+    console.log("Lower: "+userLower+"  Upper: "+userUpper+"  Sum: "+userSum+"  Quantity: "+userQuantity);
+
+    // input error handling
+    $("#input-err").css('color', 'red');
+    if (userUpper <= userLower) {
+      $("#input-err").text("lower-bound must be <= upper-bound");
+    } else if ( userQuantity >= (userUpper - userLower) ) {
+      $("#input-err").text("too many Quantity for given range");
+    } else if (userSum <= userUpper) {
+      $("#input-err").text("sum can't be less than upper bounds");
+    } else if (userQuantity >= (userUpper - userLower)) {
+      $("#input-err").text("quantity can't fit into range");
+    } else {
+      $("#input-err").css('color', 'green');
+      $("#input-err").text("input valid");
+    }
+
+    // randomize starting array
+    var baseArr = [];
+    for (i=0 ; i<userQuantity ; i++) {
+      var num = getRandomIntInclusive(userLower,userUpper);
+      baseArr.push(num);
+    }
+    console.log("baseArr = ", baseArr);
+    var baseArrSum = getSum(baseArr);
+    console.log("baseArrSum = ", baseArrSum);
+    var scaler = userSum / baseArrSum;
+    console.log("scaler = ", scaler);
+    var scaledArr = [];
+    baseArr.forEach(function(num, i) {
+      scaledArr.push(Math.round( num * scaler ));
+    });
+    console.log("scaledArr = ", scaledArr);
+    var newSum = getSum(scaledArr);
+    console.log("scaledArr sum = ", newSum);
+    var diff = 0;
+    if (newSum > userSum) {
+      diff = newSum - userSum;
+      if ((scaledArr[0] - diff) == 0) {
+        scaledArr[1] -= diff;
+      } else {
+        scaledArr[0] -= diff;
+      }
+    } else if (newSum < userSum) {
+      diff = userSum - newSum;
+      if ((scaledArr[0] + diff) == 0) {
+        scaledArr[1] += diff;
+      } else {
+        scaledArr[0] += diff;
+      }
+    } else {
+      console.log("diff is 0");
+    }
+    console.log("diff = ", diff);
+    console.log("final array sum = ", getSum(scaledArr));
   });
 
 });
